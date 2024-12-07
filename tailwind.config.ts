7048f62be34b37ac7ff328692aebe61779bf4945
gun kim <gun@whattime.co.kr>
2024-12-08 03:40:34 +0900

    initial commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,19 @@
-import type { Config } from "tailwindcss";
+import { withTV } from 'tailwind-variants/dist/transformer.js';
+import type { Config } from 'tailwindcss';
 
-export default {
+export default withTV({
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
       },
     },
   },
   plugins: [],
-} satisfies Config;
+} satisfies Config);
