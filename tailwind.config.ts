8d4e3680e918602fbf5dda56d47e043bdba6dad9
gun kim <gun@whattime.co.kr>
2024-12-13 22:43:58 +0900

    content 범위 변경

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,11 +2,7 @@ import { withTV } from "tailwind-variants/transformer";
 import type { Config } from "tailwindcss";
 
 export default withTV({
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {
       colors: {
