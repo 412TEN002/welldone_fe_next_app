70d99e986fd05c4cdc548c531ed3617758d4efa8
gun kim <gun@whattime.co.kr>
2024-12-13 22:43:20 +0900

    font variable 추가

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
     // SVG를 React 컴포넌트로 임포트 할 수 있도록 설정
     config.module.rules.push({
       test: /\.svg$/,
-      use: ['@svgr/webpack'],
+      use: ["@svgr/webpack"],
     });
 
     return config;
