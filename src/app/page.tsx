af821680caf2f76972dbb02904803bef6825fed5
PrimadonnaGit <jwhyun2215@gmail.com>
2025-01-30 15:57:14 +0900

    fix: 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,15 @@
 import { HomeTemplate } from "@/components/home/homeTemplate";
 import { categoryOption } from "@/query-options/category";
 import { cookingToolOptions } from "@/query-options/cooking-tool";
-import { integrationDetailOption, integrationOption } from "@/query-options/integration";
+import { integrationOption } from "@/query-options/integration";
 import { getQueryClient } from "@/queryClient";
 import { HydrationBoundary, dehydrate } from "@tanstack/react-query";
 
 export default async function Home() {
   const queryClient = getQueryClient();
 
-  await queryClient.prefetchQuery(categoryOption);
   await queryClient.prefetchQuery(integrationOption);
-  await queryClient.prefetchQuery(cookingToolOptions);
+  await queryClient.prefetchQuery(categoryOption);
 
   return (
     <HydrationBoundary state={dehydrate(queryClient)}>
